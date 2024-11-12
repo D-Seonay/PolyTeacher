18c4b6f3a86886def23dff0a73f61670841f855e
Mathéo DELAUNAY <dseonay@gmail.com>
2024-11-12 10:15:00 +0100

    Ajouter les routes pour la vue de contact et la classe AllTranslation

--- polyteacher/urls.py
+++ polyteacher/urls.py
@@ -16,13 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from translator.views import index
+from translator.views import index, contact
+from translator.views import AllTranslasion
 from translator.views import FrenchSpanishTranslationViewSet
 from translator.views import FrenchEnglishTranslationViewSet
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('contact', contact),
+    path('AllTranslasion', AllTranslasion.as_view(), name='AllTranslasion'),
     path('api/french_spanish_translator', FrenchSpanishTranslationViewSet.as_view(), name='french_spanish_translator'),
     path('api/french_english_translator', FrenchEnglishTranslationViewSet.as_view(), name='french_english_translator'),
 ]
